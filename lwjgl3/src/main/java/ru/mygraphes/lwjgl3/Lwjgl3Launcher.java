1b2b7531977a80282a539f8281bf47d7280f785a
ulya <149610916+fasolka12345@users.noreply.github.com>
2025-04-10 17:32:08 +0300

    Version 1.1

--- lwjgl3/src/main/java/ru/mygraphes/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/ru/mygraphes/lwjgl3/Lwjgl3Launcher.java
@@ -7,7 +7,7 @@ import ru.mygraphes.Main;
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        if (StartupHelper.startNewJvmIfRequired()) return; // This ha macOS support and helps on Windows.
         createApplication();
     }
 
